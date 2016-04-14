a1241703b3c1f2fa6e461a6fde9e4500da1a343e
dnsev-h <dnsev-h@users.noreply.github.com>
2016-04-14 19:07:30 -0400

    1.0.7.2
    Added connect tags for chrome

--- builds/eze.meta.js
+++ builds/eze.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        eze
-// @version     1.0.7.1
+// @version     1.0.7.2
 // @author      dnsev-h
 // @namespace   dnsev-h
 // @homepage    https://dnsev-h.github.io/eze/
@@ -11,6 +11,10 @@
 // @include     https://exhentai.org/*
 // @include     http://g.e-hentai.org/*
 // @include     https://g.e-hentai.org/*
+// @connect     exhentai.org
+// @connect     e-hentai.org
+// @connect     ehgt.org
+// @connect     *
 // @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwAQMAAABtzGvEAAAABlBMVEUAAABmBhHV14kpAAAAAXRSTlMAQObYZgAAADFJREFUeAFjIB4w//9BLPWBgSLq//HH/w8QQYE18GOj6hgwKCBCpcDOZQaZQpgiGgAA0dhUnSJVLdEAAAAASUVORK5CYII=
 // @updateURL   https://raw.githubusercontent.com/dnsev-h/eze/master/builds/eze.meta.js
 // @downloadURL https://raw.githubusercontent.com/dnsev-h/eze/master/builds/eze.user.js
